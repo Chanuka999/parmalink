a70b5da05945d39aed4d4abbb716beea2df40d96
Chanuka999 <chanukaranditha99@gmail.com>
2026-03-06 00:50:01 +0530

    cretae user.js model

---
+++ backend/models/user.js
@@ -0,0 +1,38 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    role: {
+      type: String,
+      enum: ["admin", "pharmacist", "cashier", "customer", "rider"],
+      default: "customer",
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
