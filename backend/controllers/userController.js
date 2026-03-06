963d146df009c36499f078ad0d8e7bc9969f46c5
Chanuka999 <chanukaranditha99@gmail.com>
2026-03-06 17:27:52 +0530

    creating login dunction

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,10 +1,13 @@
 import User from "../models/user.js";
+import bcrypt from "bcrypt";
 
 export function createUser(req, res) {
+  const hashpassword = bcrypt.hashSync(req.body.password, 10);
+
   const user = new User({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password,
+    password: hashpassword,
     role: req.body.role,
     phone: req.body.phone,
     address: req.body.address,
@@ -33,5 +36,37 @@ export function getUser(req, res) {
     })
     .catch(() => {
       console.log("not find the user");
+    });
+}
+
+export function loginUser(req, res) {
+  User.findOne({
+    email: req.body.email,
+  })
+    .then((user) => {
+      if (user == null) {
+        res.json({
+          message: "user not found",
+        });
+      } else {
+        const ispasswordMatching = bcrypt.compareSync(
+          req.body.password,
+          user.password,
+        );
+        if (ispasswordMatching) {
+          res.json({
+            message: "login successful",
+          });
+        } else {
+          res.json({
+            message: "login failed",
+          });
+        }
+      }
+    })
+    .catch((error) => {
+      res.json({
+        message: error,
+      });
     });
 }
