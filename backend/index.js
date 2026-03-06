fc58de6f01c0a40377fa38a5798bc2a7810e8f03
Chanuka999 <chanukaranditha99@gmail.com>
2026-03-06 15:19:05 +0530

    added routes

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,8 @@ import dotenv from "dotenv";
 
 import express from "express";
 import mongoose from "mongoose";
-import User from "./models/user.js";
+
+import StudentRouter from "./routes/studentRoute.js";
 
 const app = express();
 
@@ -19,42 +20,7 @@ mongoose
     console.log("database connection failed");
   });
 
-app.get("/", (req, res) => {
-  console.log(req.body);
-
-  console.log("get request");
-  let prefix = "mr";
-  if (req.body.gender == "female") {
-    prefix = "ms";
-  }
-  res.json({
-    message: "hello world" + " " + prefix + " " + req.body.name,
-  });
-});
-
-app.post("/", (req, res) => {
-  const user = new User({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password,
-    role: req.body.role,
-    phone: req.body.phone,
-    address: req.body.address,
-  });
-
-  user
-    .save()
-    .then(() => {
-      res.json({
-        message: "user added successfully",
-      });
-    })
-    .catch(() => {
-      res.json({
-        message: "user added failed",
-      });
-    });
-});
+app.use(StudentRouter);
 
 app.listen(5000, (req, res) => {
   console.log("server started on 5000");
