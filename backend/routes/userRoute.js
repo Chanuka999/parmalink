d462fbfea4870a3d4e23fdce8cde7b7872cc4300
Chanuka999 <chanukaranditha99@gmail.com>
2026-03-06 17:28:21 +0530

    add login function for routes

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,9 +1,14 @@
 import express from "express";
-import { createUser, getUser } from "../controllers/userController.js";
+import {
+  createUser,
+  getUser,
+  loginUser,
+} from "../controllers/userController.js";
 
 const UserRouter = express.Router();
 
 UserRouter.post("/user", createUser);
 UserRouter.get("/user", getUser);
+UserRouter.post("/login", loginUser);
 
 export default UserRouter;
