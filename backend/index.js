da033724b631822265a366d1242068b51ebddd8b
Chanuka999 <chanukaranditha99@gmail.com>
2026-03-07 10:36:26 +0530

    create authentication

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv";
 
 import express from "express";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 import UserRouter from "./routes/userRoute.js";
 
@@ -9,6 +10,24 @@ const app = express();
 
 app.use(express.json());
 dotenv.config();
+
+app.use((req, res, next) => {
+  let token = req.header("Authorization");
+  if (token != null) {
+    token = token.replace("Bearer ", "");
+    jwt.verify(token, "jwt-secret", (err, decorded) => {
+      if (decorded == null) {
+        res.status(404).json({
+          message: "invalid token please login again",
+        });
+        return;
+      } else {
+        req.user = decorded;
+      }
+    });
+  }
+  next();
+});
 
 const connectionString = process.env.MONGO_URL;
 mongoose
