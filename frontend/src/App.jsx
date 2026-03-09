43bdc1917ff83dc88d9f78dcee9d41bfce8e1542
Chanuka999 <chanukaranditha99@gmail.com>
2026-03-09 11:16:22 +0530

    refactor: remove unused CSS styles and update App component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import AdminPage from "./pages/AdminPage";
 
 function App() {
-  return <></>;
+  return (
+    <div className="w-full h-[100vh] bg-red-300">
+      <Routes path="/">
+        <Route path="/" element={<h1>home page</h1>} />
+        <Route path="/register" element={<h1>register page</h1>} />
+        <Route path="/adminpage" element={<AdminPage />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
