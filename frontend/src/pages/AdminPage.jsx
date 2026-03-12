3a2fc0920259de17531d1c2ebdfac21568027414
Chanuka999 <chanukaranditha99@gmail.com>
2026-03-12 22:07:18 +0530

    feat: implement AdminPage layout with routing for orders and product sections

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
 const AdminPage = () => {
-  return <div className="w-full h-full bg-blue-700"></div>;
+  return (
+    <div className="w-full h-full bg-accent flex p-2">
+      <div className="w-[300px] h-full bg-accent"></div>
+      <div className="w-[calc(100%-300px)] h-full bg-secondary rounded-[20px]">
+        <Routes path="/">
+          <Route path="/orders" element={<h1>orders</h1>} />
+          <Route path="/product" element={<h1>product</h1>} />
+        </Routes>
+      </div>
+    </div>
+  );
 };
 
 export default AdminPage;
